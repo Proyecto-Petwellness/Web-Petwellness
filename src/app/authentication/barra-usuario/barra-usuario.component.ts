142490abdfeeba20e801bc88c00be85672c16ce4
Axel <yamir@Axel.(none)>
2024-07-29 23:14:31 -0500

    Agregando el proyecto

---
+++ src/app/authentication/barra-usuario/barra-usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-usuario',
+  templateUrl: './barra-usuario.component.html',
+  styleUrl: './barra-usuario.component.css'
+})
+export class BarraUsuarioComponent {
+
+}
