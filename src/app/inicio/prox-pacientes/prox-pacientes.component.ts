d0ba1da5f6b21278743c55474f9fc84419af9990
Axel <yamir@Axel.(none)>
2024-07-29 22:06:41 -0500

    Agregando el proyecto

---
+++ src/app/inicio/prox-pacientes/prox-pacientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prox-pacientes',
+  templateUrl: './prox-pacientes.component.html',
+  styleUrl: './prox-pacientes.component.css'
+})
+export class ProxPacientesComponent {
+
+}
