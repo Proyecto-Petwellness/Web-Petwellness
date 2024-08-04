b3730a1149cec3febf0de17e4bb33efa7ab04d22
Axel <yamir@Axel.(none)>
2024-08-04 01:08:10 -0500

    feat: Estructuración de los html de cada componente del módulo de authentication

--- src/app/authentication/barra-usuario/barra-usuario.component.ts
+++ src/app/authentication/barra-usuario/barra-usuario.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-barra-usuario',
   templateUrl: './barra-usuario.component.html',
   styleUrl: './barra-usuario.component.css'
 })
-export class BarraUsuarioComponent {
-
-}
+export class BarraUsuarioComponent { }
