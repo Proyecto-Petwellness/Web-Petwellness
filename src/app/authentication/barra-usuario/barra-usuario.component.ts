c0b26c73d4cd95c7357bdeaf2b384334d5112242
Axel <yamir@Axel.(none)>
2024-08-04 22:45:33 -0500

    feat: Estilado de los componentes de barra-usuario, login, signin, verificacion e implementación de los servicios para cambiar el tipo de usuario en todo el proyecto.

--- src/app/authentication/barra-usuario/barra-usuario.component.ts
+++ src/app/authentication/barra-usuario/barra-usuario.component.ts
@@ -1,8 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { UsuarioService } from './../../services/usuario.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-barra-usuario',
   templateUrl: './barra-usuario.component.html',
   styleUrl: './barra-usuario.component.css'
 })
-export class BarraUsuarioComponent { }
+export class BarraUsuarioComponent implements OnInit {
+
+  UsuarioActivo: string = '';
+
+  constructor(private UsuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.UsuarioService.UsuarioActivo.subscribe(Usuario =>{
+      this.UsuarioActivo = Usuario;
+    })
+  }
+
+  CambiarUsuario(Usuario: string): void {
+    this.UsuarioService.SetUsuarioActivo(Usuario);
+  }
+ }
--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit, AfterViewInit {
+  
+  UsuarioActivo = '';
 
+  constructor(private UsuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.UsuarioService.UsuarioActivo.subscribe(Usuario =>{
+      this.UsuarioActivo = Usuario;
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.CambiarImagen();
+  }
+  
+  CambiarImagen(): void { 
+    const Imagen = document.getElementById("Cuerpo") as HTMLElement;
+    const Fondo = document.getElementById("Fondo") as HTMLElement;
+    switch (this.UsuarioService.GetUsuarioActivo()) {
+      case "Dueños": {
+        Imagen.style.setProperty("--fondo", `url('/assets/fondo-dueno.png')`);
+        Fondo.style.setProperty("--tamano", "1330px");
+        break;
+      }
+      case "Veterinario": {
+        Imagen.style.setProperty("--fondo", `url('/assets/fondo-vet.png')`);
+        Fondo.style.setProperty("--tamano", "1539px");
+        break;
+      }
+      case "Albergues": {
+        Imagen.style.setProperty("--fondo", `url('/assets/fondo-albergue.png')`);
+        Fondo.style.setProperty("--tamano", "1539px");
+        break;
+      }
+    }
+  }
+
+  Mostrar(): string {
+    return this.UsuarioService.GetUsuarioActivo();
+  }
+
+  OcultarContrasena(){
+    const Contrasena_1 = document.getElementById("Ver-contrasena_1") as HTMLElement;
+    const Contrasena_2 = document.getElementById("Ver-contrasena_2") as HTMLElement;
+    const tipo= Contrasena_1.getAttribute('type') === 'password' ? 'text': 'password';
+    Contrasena_1.setAttribute('type', tipo);
+    Contrasena_2.setAttribute('type', tipo);
+  }
 }
